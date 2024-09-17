ec5811af94d2c9c3700a83b626053e1587901090
mayur-sahare <mayursahare91@gmail.com>
2024-09-17 14:46:18 +0530

    radio buttons

---
+++ test/specs/uiControls.js
@@ -0,0 +1,19 @@
+describe("UI Controls Check", async () => {
+  it("radio buttons", async () => {
+    await browser.url("https://rahulshettyacademy.com/loginpagePractise/");
+    await browser.url("https://rahulshettyacademy.com/loginpagePractise/");
+    const username = $(`//input[@id='username']`);
+    await username.setValue("rahulshettyacademy");
+    const password = $(`#password`);
+    await password.setValue("learning");
+
+    //with multiple elements of same class
+    const radioButtons = $$(".radiotextsty");
+    await radioButtons[1].click();
+    await expect($(".modal-content")).toBeDisplayed();
+    await $("#cancelBtn").click();
+    //and when clicking admin is shouldn't display
+    await radioButtons[0].click();
+    await expect($(".modal-content")).not.toBeDisplayed();
+  });
+});
